cb2f38da7e4af784e49994d4688c0da6ae0dded7
spalnique <lex.sydoruk@gmail.com>
2024-05-15 21:54:43 +0200

    set-get memory test

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -3,6 +3,8 @@ import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 import { createFakeContact } from '../utils/createFakeContact.js';
 
+
+
 export const addOneContact = async () => {
   try {
     const contacts = JSON.parse(await fs.readFile(PATH_DB));
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -10,4 +10,39 @@ export const getAllContacts = async () => {
   }
 };
 
-console.log(await getAllContacts());
+class MyContacts {
+  constructor() {
+    this.contacts = [];
+    this.quantity = 0;
+  }
+  set setContacts(contacts) {
+    this.contacts = [...this.contacts, ...contacts];
+  }
+
+  get printContacts() {
+    console.log('Contacts stored in this instance:', this.contacts);
+  }
+
+  get getContacts() {
+    return this.contacts;
+  }
+
+  updateQuantity() {
+    this.quantity = this.getContacts.length;
+  }
+}
+
+const instance = new MyContacts();
+console.log('Initial instance = ', instance);
+
+instance.setContacts = await getAllContacts();
+console.log('Instance after setting contacts:');
+
+console.log('Quantity before update = ', instance.quantity);
+instance.updateQuantity();
+console.log('Quantity after update = ', instance.quantity);
+
+instance.printContacts;
+
+const canBeUsed = instance.getContacts;
+console.log('Contacts from instance stored inside a variable:', canBeUsed);
